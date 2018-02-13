87a80d8f723264ec0db770061e2fba086594b5c6
BFunk <bret.e.funk@gmail.com>
2018-02-12 19:08:20 -0500

    more tests

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -12,66 +12,70 @@ contract CampaignFactory {
     address newCampaign = new Campaign(_description, _moneyGoal, _timeGoal, takeoff, msg.sender);
     campaigns.push(newCampaign);
   }
-}
 
-contract Campaign {
-  address public takeoff;
-  // the owner is the escrow
-  address public owner;
-  string public description;
-  uint public moneyGoal;
-  uint public timeGoal;
-  Contributor[] public contributors;
-  uint public balance;
-  uint public start;
-
-  struct Contributor {
-    address _address;
-    uint _amountGiven;
+  function allCampaigns() public view returns (address[]) {
+    return campaigns;
+  }
   }
 
-  function Campaign(string _description, uint _moneyGoal, uint _timeGoal, address _takeoff, address _owner) public  {
-    takeoff = _takeoff;
-    owner = _owner;
-    description = _description;
-    moneyGoal = _moneyGoal;
-    timeGoal = _timeGoal;
-    start = now;
-  }
+  contract Campaign {
+    address public takeoff;
+    // the owner is the escrow
+    address public owner;
+    string public description;
+    uint public moneyGoal;
+    uint public timeGoal;
+    Contributor[] public contributors;
+    uint public balance;
+    uint public start;
 
-  function contribute() public payable {
-    balance += msg.value;
-    contributors.push(Contributor(msg.sender, msg.value));
-  }
-
-  modifier timeCheck() {
-    require((start + timeGoal) < now);
-    _;
-  }
-
-  function disperseFunds() public payable timeCheck {
-    uint fee = balance / 100;
-    if (balance >= moneyGoal) {
-      // get out 2% fee and gas money
-      owner.transfer(fee);
-      balance = balance - (fee * 2);
-      owner.transfer(balance);
-      balance = 0;
-    } else {
-      for(uint i = 0; i < contributors.length; i++) {
-        // get out 2% fee and gas money
-        contributors[i]._address.transfer(contributors[i]._amountGiven / 100);
-      }
-      takeoff.transfer(fee);
-      balance = 0;
+    struct Contributor {
+      address _address;
+      uint _amountGiven;
     }
+
+    function Campaign(string _description, uint _moneyGoal, uint _timeGoal, address _takeoff, address _owner) public  {
+      takeoff = _takeoff;
+      owner = _owner;
+      description = _description;
+      moneyGoal = _moneyGoal;
+      timeGoal = _timeGoal;
+      start = now;
+    }
+
+    function contribute() public payable {
+      balance += msg.value;
+      contributors.push(Contributor(msg.sender, msg.value));
+    }
+
+    modifier timeCheck() {
+      require((start + timeGoal) < now);
+      _;
+    }
+
+    function disperseFunds() public payable timeCheck {
+      uint fee = balance / 100;
+      if (balance >= moneyGoal) {
+        // get out 2% fee and gas money
+        owner.transfer(fee);
+        balance = balance - (fee * 2);
+        owner.transfer(balance);
+        balance = 0;
+      } else {
+        for(uint i = 0; i < contributors.length; i++) {
+          // get out 2% fee and gas money
+          contributors[i]._address.transfer(contributors[i]._amountGiven / 100);
+        }
+        takeoff.transfer(fee);
+        balance = 0;
+      }
+    }
+
+    // function ownerBalance() public view returns(uint) {
+    //     return owner.balance;
+    // }
+
+    // function takeoffBalance() public view returns(uint) {
+    //     return takeoff.balance;
+    // }
   }
-
-  // function ownerBalance() public view returns(uint) {
-  //     return owner.balance;
-  // }
-
-  // function takeoffBalance() public view returns(uint) {
-  //     return takeoff.balance;
-  // }
-}
--- test/campaign.js
+++ test/campaign.js
@@ -5,16 +5,18 @@ const provider = ganache.provider();
 const Web3 = require('web3')
 const web3 = new Web3(provider);
 const compiledFactory = require('../ethereum/build/CampaignFactory.json')
+const compiledCampaign = require('../ethereum/build/Campaign.json')
 
 let accounts;
 let factory;
 let campaign;
+let campaignAddress;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-  .deploy({ data: compiledFactory.bytecode} )
-  .send({ from: accounts[0], gas: '1000000' })
+    .deploy({ data: compiledFactory.bytecode} )
+    .send({ from: accounts[0], gas: '1000000' })
 
   factory.setProvider(provider);
 
@@ -23,8 +25,14 @@ beforeEach(async () => {
     "test campaign to raise money", 5000000, 5
   ).send({ from: accounts[1], gas: 3000000 })
 
-  const campaignAddress = await factory.methods.campaigns(0).call()
-  //campaign.setProvider(provider);
+  campaignAddress = await factory.methods.campaigns(0).call()
+  //
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface
+    ), campaignAddress);
+
+  await campaign.setProvider(provider);
+  //console.log(campaign);
 })
 
 describe('Campaign', async () => {
@@ -33,11 +41,47 @@ describe('Campaign', async () => {
   })
 
   it('has a factory', async () => {
-    assert.ok(factory);
+    assert.ok(await factory);
   })
 
   it('has a campaign', async () => {
-    assert.ok(campaign);
+    assert.ok(await campaign);
+  })
+
+  it('factory has a list of campaigns', async () => {
+    const allCampaigns = await factory.methods.allCampaigns().call();
+    assert.equal(allCampaigns.length, 1);
+  })
+
+  it('contributor can contribute to a campaign', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[2],
+      gas: '1000000'
+    })
+
+    //correct way to access values?
+    //const balance = campaign.values.balance().call()
+    //assert.equal(balance, 1000000)
+
+    //const contributors = campaign.methods.contributors().get()
+    ////got zero params expected one?
+    //assert.equal(contributors.length, 1);
+  })
+
+  it('', async () => {
+
+  })
+
+  it('', async () => {
+
+  })
+
+  it('', async () => {
+
+  })
+
+  it('', async () => {
+
   })
 })
 
